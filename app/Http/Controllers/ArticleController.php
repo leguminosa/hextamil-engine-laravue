90e087978bc1702cd5ec88d8ca435dd5c0f6faf5
Martin Johnsons <martinjohnsons23@gmail.com>
2020-10-11 19:22:43 +0700

    add controller, resource, and model for article table

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\ArticleModel;
+use App\Http\Resources\ArticleResource;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // get articles
+        $articles = ArticleModel::paginate(10);
+
+        // return as resource collection
+        return ArticleResource::collection($articles);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $article = $request->isMethod('put') ? ArticleModel::findOrFail($request->article_id) : new ArticleModel;
+
+        // $article->id = $request->input('article_id');
+        $article->name = $request->input('name');
+        $article->title = $request->input('title');
+        $article->body = $request->input('body');
+
+        if($article->save()) {
+            return new ArticleResource($article);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // get single article
+        $article = ArticleModel::findOrFail($id);
+
+        // return as resource
+        return new ArticleResource($article);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $article = ArticleModel::findOrFail($id);
+
+        if($article->delete()) {
+            return new ArticleResource($article);
+        }
+    }
+}
